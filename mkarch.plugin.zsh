0c2259dd34994d4f004fda0e04a38469d2d899f2
0rtz <46892933+0rtz@users.noreply.github.com>
2023-09-15 21:04:58 +0300

    Conform to Zsh-Plugin-Standard/Functions-Directory

--- mkarch.plugin.zsh
+++ mkarch.plugin.zsh
@@ -1,1 +1,8 @@
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ ${zsh_loaded_plugins[-1]} != */mkarch && -z ${fpath[(r)${0:h}/functions]} ]] {
+    fpath+=( "${0:h}/functions" )
+}
+
 autoload -Uz mkarch
